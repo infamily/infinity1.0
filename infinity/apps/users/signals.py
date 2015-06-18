18dda81defbb2c34d26bce47759cc1168f589e75
Ali Aliyev <shamkir@gmail.com>
2015-06-18 11:48:23 +0500

    added user signal

--- infinity/apps/users/signals.py
+++ infinity/apps/users/signals.py
@@ -1,6 +1,10 @@
 def user_post_save(sender, instance, created, *args, **kwargs):
-    """
-    Get username from email
+    """ Get username from email
+    sender - The model class.
+    instance - The actual instance being saved.
+    created - Boolean; True if a new record was created.
+
+    *args, **kwargs - Capture the unneeded `raw` and `using` arguments.
     """
     if created:
         username = instance.email.split('@')[0]
