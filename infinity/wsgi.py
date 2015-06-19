5a28f1c5635b217a0b8d8df130e1881d2a6e9bcd
Iurii Kriachko <yuriykryachko@gmail.com>
2015-06-19 11:33:10 +0000

    updated wsgi

--- infinity/wsgi.py
+++ infinity/wsgi.py
@@ -25,7 +25,7 @@ path.append(SITE_ROOT)
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "jajaja.settings"
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infinity.settings.production")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infinity.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infinity.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
