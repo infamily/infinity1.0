a9abd0b9c77be2b8c6eade4a9cc6d161c7b07e81
Ali Aliyev <shamkir@gmail.com>
2015-06-19 16:09:27 +0500

    added sendgrid email backend for production

--- infinity/settings/production.py
+++ infinity/settings/production.py
@@ -29,28 +29,9 @@ ALLOWED_HOSTS = ['.infinity.7webpages.com']
 
 # EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-# EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-# EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-# EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-# EMAIL_PORT = environ.get('EMAIL_PORT', 587)
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-# EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
-# EMAIL_USE_TLS = True
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-# SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = "sgbackend.SendGridBackend"
+SENDGRID_USER = "ironcoder-demo"
+SENDGRID_PASSWORD = "123456789a"
 # END EMAIL CONFIGURATION
 
 # DATABASE CONFIGURATION
