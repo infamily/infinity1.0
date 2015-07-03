e603399bb19d7ad480feac5cfd71a831ac3243ab
maximzxc <maximzxc@gmail.com>
2015-07-03 21:11:14 +0300

    fixtures for languages added

--- infinity/apps/core/migrations/0007_auto_20150703_0801.py
+++ infinity/apps/core/migrations/0007_auto_20150703_0801.py
@@ -2,6 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.core.management import call_command
+
+
+def loadfixture(apps, schema_editor):
+    call_command('loaddata', 'languages.json')
 
 
 class Migration(migrations.Migration):
@@ -19,4 +24,5 @@ class Migration(migrations.Migration):
             name='need',
             unique_together=set([('language', 'name', 'definition')]),
         ),
+        migrations.RunPython(loadfixture),
     ]
