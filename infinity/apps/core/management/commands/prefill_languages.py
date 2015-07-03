b44665d0abb494ab9422ad2963e05772248ef843
maximzxc <maximzxc@gmail.com>
2015-07-03 13:08:28 +0300

    fix

--- infinity/apps/core/management/commands/prefill_languages.py
+++ infinity/apps/core/management/commands/prefill_languages.py
@@ -13,10 +13,10 @@ class Command(BaseCommand):
 
     def prefill(self):
         db = MySQLdb.connect(host="localhost", user="root", passwd="pwd",
-                             db="omegawiki")
+                             db="omegawiki", charset='utf8')
         cur = db.cursor()
-        cur.execute("select language_id, language_name from language_names where name_language_id = 85;")
-        # Language.objects.all().delete()
+        cur.execute("select language_id, language_name from language_names where name_language_id = language_id;")
+        Language.objects.all().delete()
         for row in cur.fetchall():
             Language.objects.create(omegawiki_language_id=int(row[0]),
-                                    name=row[1].decode('utf-8', 'ignore'))
+                                    name=row[1])
