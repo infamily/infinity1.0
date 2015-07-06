1e8018d2010b200e802594a496dd22eddfc0d003
maximzxc <maximzxc@gmail.com>
2015-07-06 18:22:34 +0300

    fixes infinity

--- infinity/apps/core/forms.py
+++ infinity/apps/core/forms.py
@@ -351,7 +351,7 @@ class NeedCreateForm(forms.ModelForm):
             Div(
                 Div('language', css_class='col-sm-2',),
                 Div(
-                    Field('name', placeholder=kwargs.pop('query_placeholder', 'Name'), onkeyup="searchOpen()"),
+                    Field('name', placeholder=kwargs.pop('query_placeholder', 'Name'), oninput="searchOpen()"),
                     css_class='col-sm-10',
                 ),
                 css_class='row'
