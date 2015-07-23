99f498e320e3504286577c42bf278e43a57949dd
Ali Aliyev <shamkir@gmail.com>
2015-07-23 17:40:42 +0500

    added group signal

--- infinity/apps/users/signals.py
+++ infinity/apps/users/signals.py
@@ -27,5 +27,5 @@ def user_post_save(sender, instance, created, *args, **kwargs):
     from django.contrib.auth.models import Group
     if created:
         group, group_created = Group.objects.get_or_create(name='invite')
-        instance.groupd.add(group)
+        instance.group.add(group)
         instance.save()
