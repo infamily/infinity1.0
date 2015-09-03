28abfaf74eec391622a97161c8d7398058e13298
Mindey <mindey@qq.com>
2015-09-03 15:34:20 +0300

    added basic IndexView

--- infinity/apps/core/views.py
+++ infinity/apps/core/views.py
@@ -10,6 +10,7 @@ from django.views.generic import DetailView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 from pure_pagination.mixins import PaginationMixin
@@ -24,6 +25,21 @@ from .utils import ViewTypeWrapper
 from .models import *
 from .forms import *
 from .filters import *
+
+class IndexView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = {
+            'goal_list': Goal.objects.order_by('-id')[:5],
+            'idea_list': Idea.objects.order_by('-id')[:5],
+            'plan_list': Plan.objects.order_by('-id')[:5]
+        }
+
+        context.update(kwargs)
+
+        return context
 
 
 class AjaxChainedView(ChainedSelectChoicesView):
