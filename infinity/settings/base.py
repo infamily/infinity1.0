36ab87b65e0eec1f3a7b394390394b692bc49ebe
Ali Aliyev <shamkir@gmail.com>
2015-09-24 02:09:20 +0500

    fixed markitup width

--- infinity/settings/base.py
+++ infinity/settings/base.py
@@ -99,7 +99,7 @@ STATICFILES_DIRS = (
     normpath(join(DJANGO_ROOT, 'static')),
 )
 
-MARKDOWN_SET_PATH = normpath(join(DJANGO_ROOT, 'static/markdown'))
+MARKDOWN_SET_PATH = normpath(join(DJANGO_ROOT, 'static'))
 
 # See:
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
