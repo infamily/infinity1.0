a72c6feb4b6092175a60ae7491d9711511bd3ad4
Ali Aliev <shamkir@gmail.com>
2016-01-07 17:40:05 +0400

    get private data from environment

--- infinity/settings/local_example.py
+++ infinity/settings/local_example.py
@@ -1,9 +1,6 @@
 """Development settings and globals."""
 
-from __future__ import absolute_import
-
-from os.path import join, normpath
-
+import os
 from .base import *
 
 
@@ -15,44 +12,44 @@ DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # END DEBUG CONFIGURATION
 
-
 # EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 if DEBUG:
+    # If debug is enabled this means we use the project locally
+    # let's allow Django to display the letters in the console
     DEFAULT_FROM_EMAIL = "noreply@infty.xyz"
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    DEFAULT_FROM_EMAIL = "noreply@infty.xyz"
+    # If DEBUG is disabled send letters throug sendgrid
     EMAIL_BACKEND = "sgbackend.SendGridBackend"
-    SENDGRID_USER = "ironcoder-demo"
-    SENDGRID_PASSWORD = "123456789a"
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@infty.xyz")
+    SENDGRID_USER = os.getenv("SENDGRID_USER", "ironcoder-demo")
+    SENDGRID_PASSWORD = os.getenv("SENDGRID_PASSWORD", "123456789a")
 # END EMAIL CONFIGURATION
-
 
 # DATABASE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'infty',
-        'USER': 'aliev',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': os.getenv("DATABASE_NAME", "infty"),
+        'USER': os.getenv("DATABASE_USER", ""),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD", ""),
+        'HOST': os.getenv("DATABASE_HOST", ""),
+        'PORT': os.getenv("DATABASE_PORT", "")
     }
 }
 # END DATABASE CONFIGURATION
 
-
 # CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+if not DEBUG:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        }
     }
-}
 # END CACHE CONFIGURATION
-
 
 # TOOLBAR CONFIGURATION
 # See:
