ab25e07dd44f77588a638a1651c8ca8dfc747224
Mindey <mindey@qq.com>
2016-02-11 05:21:12 +0200

    also added the translation of content item itself, for convenience

--- src/static/js/comment_translate.js
+++ src/static/js/comment_translate.js
@@ -13,11 +13,11 @@ function get_translations() {
       type : "POST",
       data : json,
       success : function(json) {
-          console.log(json);
           for (var key in json) {
             $('#'+key.replace('source','translation')).html(json[key]);
-            console.log(key + ' -> ' + json[key]);
           }
+          $('#translation-title').find('h1').css('color','#36648b');
+          $('#translation-subtitle').find('h2').css('color','#36648b');
       },
       error : function(xhr,errmsg,err) {
           console.log(xhr.status + ": " + xhr.responseText);
